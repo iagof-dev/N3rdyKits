3afd2a722036f331d914fd0eed26c9b2cd1b0eb2
N3rdy <n3rdydzn@hotmail.com>
2023-06-06 11:26:49 -0300

    WIP & Fixes

--- src/main/java/com/n3rdydev/events/listener.java
+++ src/main/java/com/n3rdydev/events/listener.java
@@ -30,7 +30,6 @@ public class listener implements Listener {
         e.setJoinMessage("");
 
         Location spawn_loc = new Location(p.getWorld(),  spawn.spawn_x, spawn.spawn_y, spawn.spawn_z);
-
         p.teleport(spawn_loc);
         p.setHealth(20);
         com.n3rdydev.kits.spawn.Receive(p);
@@ -42,30 +41,17 @@ public class listener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
+    public void PlayerRespawnEvent(PlayerRespawnEvent e){
+        Player p = e.getPlayer();
+        Location spawn_loc = new Location(p.getWorld(),  spawn.spawn_x, spawn.spawn_y, spawn.spawn_z);
+        e.setRespawnLocation(spawn_loc);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerDeath(PlayerDeathEvent e) {
         Player p = (Player) e.getEntity().getPlayer();
         e.setDeathMessage("");
-
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                for (ItemStack drop : e.getDrops()) {
-                    drop.setAmount(0);
-                }
-                e.getDrops().clear();
-                p.playSound(p.getLocation(), Sound.LAVA_POP, 1, 1);
-            }
-        }.runTaskLater(com.n3rdydev.main.getPlugin(), 60L);
-
-        e.getEntity().spigot().respawn();
-        p.getInventory().clear();
-        p.updateInventory();
-
-        Location spawn_loc = new Location(p.getWorld(),  spawn.spawn_x, spawn.spawn_y, spawn.spawn_z);
-        p.teleport(spawn_loc);
-
-        com.n3rdydev.kits.spawn.Receive(p);
-        p.updateInventory();
+        e.getDrops().clear();
 
         if (e.getEntity().getKiller() == null) {
             p.sendMessage("§cVocê morreu!");
@@ -75,39 +61,61 @@ public class listener implements Listener {
             pk.sendMessage("§a Você matou " + p.getDisplayName() + "! (+5 xp)");
         }
 
+        e.getEntity().spigot().respawn();
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                com.n3rdydev.kits.spawn.Receive(p);
+            }
+        }.runTaskLater(com.n3rdydev.main.getPlugin(), 1L);
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerQuit(PlayerQuitEvent e) {
-        Player p = (Player) e.getPlayer();
         e.setQuitMessage("");
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onplayerMove(PlayerMoveEvent e) {
-        Player p = e.getPlayer();
-        Location p_loc = p.getLocation();
 
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void PlayerHunger(FoodLevelChangeEvent e) {
-        e.setCancelled(true);
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void PlayerDrop(PlayerDropItemEvent e) {
         Player p = (Player) e.getPlayer();
         Material item_drop = e.getItemDrop().getItemStack().getType();
-        if (item_drop == Material.DIAMOND_SWORD || item_drop == Material.IRON_SWORD || item_drop == Material.STONE_SWORD || item_drop == Material.WOOD_SWORD || item_drop == Material.GOLD_SWORD) {
+
+        boolean is_sword = false;
+
+        switch(item_drop){
+            case DIAMOND_SWORD:
+                is_sword = true;
+                break;
+            case IRON_SWORD:
+                is_sword = true;
+                break;
+            case GOLD_SWORD:
+                is_sword = true;
+                break;
+            case STONE_SWORD:
+                is_sword = true;
+                break;
+            case WOOD_SWORD:
+                is_sword = true;
+                break;
+            case CHEST:
+                e.setCancelled(true);
+                break;
+            case COMPASS:
+                e.setCancelled(true);
+                break;
+        }
+
+        if(is_sword){
             e.setCancelled(true);
             p.playSound(p.getLocation(), Sound.ITEM_BREAK, 1, 1);
             p.sendMessage("§cCuidado guerreiro, não drope seu armamento...");
-            return;
-        }
-
-        if(item_drop == Material.CHEST){
-            e.setCancelled(true);
             return;
         }
 
@@ -127,8 +135,13 @@ public class listener implements Listener {
         if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_BLOCK) {
             if (e.getItem() != null && e.getItem().getType().equals(Material.CHEST)) {
                 p.openInventory(com.n3rdydev.gui.kits.list_kits(p));
+            }
+            else{
                 e.setCancelled(false);
             }
+        }
+        else{
+            e.setCancelled(false);
         }
     }
 
@@ -136,13 +149,56 @@ public class listener implements Listener {
     public void onInventoryClick(InventoryClickEvent e) {
         Player p = (Player) e.getWhoClicked();
         if (e.getView().getTitle().equals("Lista de Kits")) {
+            e.setCancelled(true);
             switch (e.getCurrentItem().getType()) {
                 case DIAMOND_SWORD:
                     p.closeInventory();
                     com.n3rdydev.kits.PvP.Receive(p);
                     break;
+                case FIREWORK:
+                    break;
             }
         }
+        else{
+            e.setCancelled(false);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
+        if (e.getDamager() instanceof Player && e.getEntity() instanceof Player) {
+            Player attacker = (Player) e.getDamager();
+            Player target = (Player) e.getEntity();
+
+            if (is_safe_zone(e.getEntity().getLocation(), -28, 378, -34, 384)) {
+                e.setCancelled(true);
+            }
+        }
+    }
+
+
+    private boolean is_safe_zone(Location location, int x1, int z1, int x2, int z2) {
+        int minX = Math.min(x1, x2);
+        int minZ = Math.min(z1, z2);
+        int maxX = Math.max(x1, x2);
+        int maxZ = Math.max(z1, z2);
+
+        int locX = location.getBlockX();
+        int locZ = location.getBlockZ();
+
+        return locX >= minX && locX <= maxX && locZ >= minZ && locZ <= maxZ;
+    }
+
+    private boolean calc_between(Location location, int x1, int z1, int x2, int z2) {
+        int minX = Math.min(x1, x2);
+        int minZ = Math.min(z1, z2);
+        int maxX = Math.max(x1, x2);
+        int maxZ = Math.max(z1, z2);
+
+        int locX = location.getBlockX();
+        int locZ = location.getBlockZ();
+
+        return locX >= minX && locX <= maxX && locZ >= minZ && locZ <= maxZ;
     }
 
 
@@ -161,6 +217,11 @@ public class listener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
+    public void PlayerHunger(FoodLevelChangeEvent e) {
+        e.setCancelled(true);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerPlaceBlocks(BlockPlaceEvent e) {
         if (e.getPlayer().hasPermission("n3rdydev.0000.buildon")) {
             e.setCancelled(false);
@@ -173,11 +234,10 @@ public class listener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void PlayerItemDamage(PlayerItemDamageEvent e) {
         ItemStack item = e.getItem();
-        if (item.getType() == Material.DIAMOND_SWORD || item.getType() == Material.IRON_SWORD || item.getType() == Material.STONE_SWORD || item.getType() == Material.GOLD_SWORD || item.getType() == Material.WOOD_SWORD) {
-
-        }
 
     }
+
+
 
 
     @EventHandler(priority = EventPriority.HIGHEST)
--- src/main/java/com/n3rdydev/events/soup.java
+++ src/main/java/com/n3rdydev/events/soup.java
@@ -4,24 +4,42 @@ import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class soup extends listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onInteract(PlayerInteractEvent e) {
         Player p = (Player) e.getPlayer();
-        if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if(p.getInventory().getItemInHand().getType() == Material.MUSHROOM_SOUP && p.getHealth() < 20) {
+        if (e.getItem() != null && e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if (p.getInventory().getItemInHand().getType() == Material.MUSHROOM_SOUP && p.getHealth() < 20) {
                 e.setCancelled(true);
-                ItemStack pote = new ItemStack(Material.BOWL, 1);
-                p.getInventory().setItemInHand(pote);
-                p.playSound(p.getLocation(), Sound.EAT, 1, 1);
-                p.setHealth(Math.min(20, p.getHealth() + 8));
-                p.updateInventory();
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+
+                        ItemStack pote = new ItemStack(Material.BOWL, 1);
+                        ItemMeta pote_meta = pote.getItemMeta();
+                        pote_meta.setDisplayName("Pote Vazio");
+                        pote.setItemMeta(pote_meta);
+
+                        p.playSound(p.getLocation(), Sound.EAT, 1, 1);
+                        p.setHealth(Math.min(20, p.getHealth() + 8));
+
+                        p.getInventory().setItemInHand(pote);
+                        p.updateInventory();
+                    }
+
+                }.runTaskLater(com.n3rdydev.main.getPlugin(), 1L);
+
             }
+        } else {
+            e.setCancelled(false);
         }
     }
 
