8583722ced9424fa0ef13acc47ffe4cdb9824b1d
N3rdy <n3rdydzn@hotmail.com>
2023-06-08 23:39:59 -0300

    WIP | Add Kit archer

---
+++ src/main/java/com/n3rdydev/kits/Archer.java
@@ -0,0 +1,65 @@
+package com.n3rdydev.kits;
+
+import com.n3rdydev.events.handleArenaTeleport;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class Archer {
+    public static void Receive(Player p) {
+        if (p.hasPermission("n3rdydev.kit.archer") || p.hasPermission("n3rdydev.kit.*")) {
+            p.getInventory().clear();
+
+            ItemStack sword = new ItemStack(Material.STONE_SWORD, 1);
+            ItemStack soup = new ItemStack(Material.MUSHROOM_SOUP, 1);
+            ItemStack r_mushroom = new ItemStack(Material.RED_MUSHROOM, 64);
+            ItemStack b_mushroom = new ItemStack(Material.BROWN_MUSHROOM, 64);
+            ItemStack bowl = new ItemStack(Material.BOWL, 64);
+
+            ItemStack bow = new ItemStack(Material.BOW, 1);
+            bow.addEnchantment(Enchantment.ARROW_INFINITE, 1);
+            ItemStack arrow = new ItemStack(Material.ARROW, 1);
+
+            sword.addEnchantment(Enchantment.DAMAGE_ALL, 1);
+
+            ItemMeta sopa = (ItemMeta) soup.getItemMeta();
+            sopa.setDisplayName("§eSopa Mágica");
+            soup.setItemMeta(sopa);
+
+            for (int z = 0; z <= 36; z++) {
+
+                switch (z) {
+                    case 0:
+                        p.getInventory().addItem(sword);
+                        break;
+                    case 1:
+                        p.getInventory().addItem(bow);
+                        break;
+                    case 13:
+                        p.getInventory().addItem(r_mushroom);
+                        break;
+                    case 14:
+                        p.getInventory().addItem(bowl);
+                        break;
+                    case 15:
+                        p.getInventory().addItem(b_mushroom);
+                        break;
+                    case 19:
+                        p.getInventory().addItem(arrow);
+                        break;
+                    default:
+                        p.getInventory().addItem(soup);
+                        break;
+
+                }
+            }
+            p.updateInventory();
+            p.teleport(handleArenaTeleport.random_tp(p));
+        } else {
+            p.sendMessage("§cVocê não possui o kit Archer.");
+            return;
+        }
+    }
+}
