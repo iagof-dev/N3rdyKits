354f952b3cde271d3a1130fe0ec735819353d0db
N3rdy <n3rdydzn@hotmail.com>
2023-07-03 00:18:23 -0300

    WIP | bruh

---
+++ src/main/java/com/n3rdydev/commands/Suicidar.java
@@ -0,0 +1,15 @@
+package com.n3rdydev.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Suicidar implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        Player p = (Player) commandSender;
+        p.setHealth(0);
+        return true;
+    }
+}
